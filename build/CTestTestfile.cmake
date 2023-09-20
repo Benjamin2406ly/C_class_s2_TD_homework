4fa3f263420d539eb82f682280d68051f1ad43f9
Benjamin2406ly <112814260+Benjamin2406ly@users.noreply.github.com>
2023-09-20 21:13:59 +0800

    refactor:C_class_s2_task5_Frame

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/vscode/C_class_s2_TD_homework
+# Build directory: C:/vscode/C_class_s2_TD_homework/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
